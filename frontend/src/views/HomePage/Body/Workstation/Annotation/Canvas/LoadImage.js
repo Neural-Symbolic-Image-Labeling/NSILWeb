bd08f7bc85877547fd818bfa039eaeed9bdd0f11
bx <edlislbx@yeah.net>
2022-08-01 18:16:13 -0400

    fix image display position bug

--- frontend/src/views/HomePage/Body/Workstation/Annotation/Canvas/LoadImage.js
+++ frontend/src/views/HomePage/Body/Workstation/Annotation/Canvas/LoadImage.js
@@ -18,15 +18,21 @@ const LoadImage = ({ imageUrl, onMouseDown }) => {
       const { naturalHeight, naturalWidth} = image;
       console.log(naturalHeight, naturalWidth);
       if (naturalWidth > naturalHeight){
-        let ratio = naturalHeight >= 600 ? 1-((naturalHeight-600)/naturalHeight) : ((600 -naturalHeight)/naturalHeight)+1
-        image.width = Math.ceil(naturalWidth*ratio);
-        image.height = Math.ceil(naturalHeight*ratio);
-        image.alt = Math.ceil(450-((naturalWidth*ratio)/2));
-      }else{
         let ratioH = naturalWidth >= 900 ? 1-((naturalWidth-900)/naturalWidth) : ((900-naturalWidth)/naturalWidth)+1
         image.width = Math.ceil(naturalWidth*ratioH);
         image.height = Math.ceil(naturalHeight*ratioH);
         image.alt = Math.ceil(450-((naturalWidth*ratioH)/2));
+        if(image.height > 600){
+          let ratio = naturalHeight >= 600 ? 1-((naturalHeight-600)/naturalHeight) : ((600 -naturalHeight)/naturalHeight)+1
+          image.width = Math.ceil(naturalWidth*ratio);
+          image.height = Math.ceil(naturalHeight*ratio);
+          image.alt = Math.ceil(450-((naturalWidth*ratio)/2));
+        }
+      }else{
+        let ratio = naturalHeight >= 600 ? 1-((naturalHeight-600)/naturalHeight) : ((600 -naturalHeight)/naturalHeight)+1
+        image.width = Math.ceil(naturalWidth*ratio);
+        image.height = Math.ceil(naturalHeight*ratio);
+        image.alt = Math.ceil(450-((naturalWidth*ratio)/2));
       }
       console.log(image.width, image.height);
       setImage(image);
